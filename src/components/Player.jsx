cbd7c2df8018958b672314642791cac2e5615e66
jigneshD2405 <jignesh.dudharejiya@team.mightytech.io>
2025-01-06 17:10:30 +0530

    dashboard setting

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,19 +1,10 @@
-import { useState, useRef } from "react";
 export default function Player() {
-  const playerInput = useRef()
-  const [enteredPlayerName, setEnteredPlayerName] = useState(null)
-
-  const handleClick = () => {
-    setEnteredPlayerName(playerInput.current.value)
-    playerInput.current.value = ""
-  }
-
   return (
     <section id="player">
-      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
+      <h2>Welcome unknown entity</h2>
       <p>
-        <input type="text" ref={playerInput} />
-        <button onClick={handleClick}>Set Name</button>
+        <input type="text" />
+        <button>Set Name</button>
       </p>
     </section>
   );
